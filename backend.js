be917dd6137f7ff064e73b960b1e014ab2ae59aa
Ujjwal <guptarickey3@gmail.com>
2023-07-18 01:12:18 +0530

    generator added

--- backend.js
+++ backend.js
@@ -18,9 +18,41 @@ Array.from(buttons).forEach((button)=>{
             document.querySelector("textarea").innerText = string;
         }
         else{
+            if(e.target.innerHTML!="Next"){
             console.log(e.target);
             string = string + e.target.innerHTML;
             document.querySelector("textarea").innerText = string;
+            }
         }
     })
+})
+
+
+let btn=document.getElementById('btn');
+let output=document.getElementById('output');
+
+let quotes = [
+"There’s an enzyme named after Pikachu",
+"The creator of Pokémon was inspired by insect-catching",
+"Raikou is the only legendary Pokemon Ash hasn't encountered in the anime or in one of the movies.",
+"One of Parasect's Pokedex entries says its spores are used as medicine in China, which is a rare reference to the real world.",
+
+"When the show was first being planned, Ash was originally going to be given a Clefairy. It was switched out at the last minute with Pikachu.",
+"Koffing & Weezing Used To Be Named After American Cities",
+"Machamp Isn't Wearing Underwear",
+"There Are No Fighting/Electric Type Pokemon",
+"Rayquaza Eats Meteorites",
+"There Are No Bug-Type Legendary Pokemon",
+"Drampa Is A Chinese Dragon, Specifically A Zhulong ",
+"Suicune Is Based On A Kirin, A Chinese Unicorn",
+"People Eat Pokémon",
+"It wears the skull of its dead mother on its head . When it becomes lonesome its said to cry loudly .",
+
+
+];
+
+
+btn.addEventListener('click',function(){
+    var randomQuote=quotes[Math.floor(Math.random()*quotes.length)];
+    output.innerHTML=randomQuote;
 })
